dd257983c942b11adfeb1943a6fd4ee7f60d9e30
Vogusov <vogusov@gmail.com>
2020-09-28 23:29:32 +0300

    Trait singleton + Products.

--- index.php
+++ index.php
@@ -1,50 +1,81 @@
 <?php
-//
-//abstract class Product {
-//  const TAX = 0.13;
-//  abstract public function getFinalPrice();
-//  abstract public function countProfit();
-//}
-//
-//class DigitalProduct extends Product {
-//  const PRICE = 1000;
-//  public function getFinalPrice() {
-//    return PieceProduct::getFinalPrice() / 2;
-//  }
-//
-//  public function countProfit() {
-//    // TODO: Implement countProfit() method.
-//  }
-//}
-//
-//class PieceProduct extends Product {
-//  private $price;
-//  private $quantity = 1;
-//
-//  public function __construct($price) {
-//    $this->price = $price;
-//
-//  }
-//
-//
-//  public function getFinalPrice() {
-//    return $this->price * $this->quantity;
-//  }
-//}
-//
-//class WeightProduct extends Product {
-//
-//  private $price;
-//  private $weight;
-//
-//  public function __construct($price, $weight) {
-//    $this->price = $price;
-//    $this->weight = $weight;
-//  }
-//
-//  public function getFinalPrice() {
-//    return $this->price * $this->weight;
-//  }
-//
-//
-//}
+
+abstract class Product {
+
+  const TAX = 0.13;      // налог с продаж
+  const OVERPRICE = 0.3; // наценка на товар (уже включена в цену)
+
+  abstract public function getPrice();
+
+  abstract public function getFinalPrice();
+
+  public function countProfit() {
+    return $this->getFinalPrice() * (1 - self::TAX) * self::OVERPRICE;
+  }
+
+}
+
+
+class DigitalProduct extends Product {
+
+  const PRICE = 1000;
+  private $quantity;    // кол-во товара
+
+  public function __construct(int $quantity) {
+    $this->quantity = $quantity;
+  }
+
+  public function getPrice() {
+    return self::PRICE;
+  }
+
+  public function getQuantity() {
+    return $this->quantity;
+  }
+
+  public function getFinalPrice() {
+    return self::PRICE * $this->quantity;
+  }
+
+}
+
+class PieceProduct extends DigitalProduct {
+
+  public function getPrice() {
+    return parent::PRICE * 2;
+  }
+
+  public function getFinalPrice() {
+    return $this->getPrice() * $this->getQuantity();
+  }
+
+
+}
+
+class WeightProduct extends Product {
+
+  private $price;
+  private $amount;
+
+  public function __construct(float $price, float $amount) {
+    $this->price = $price;
+    $this->amount = $amount;
+  }
+
+  public function getPrice() {
+    return $this->price;
+  }
+
+  public function getFinalPrice() {
+    return $this->price * $this->amount;
+  }
+}
+
+$pr = new DigitalProduct(5);
+echo 'Final profit of digital product is: ' . $pr->countProfit() . PHP_EOL;
+
+$pr = new PieceProduct(5);
+echo 'Final profit of piece product is: ' . $pr->countProfit() . PHP_EOL;
+
+$pr = new WeightProduct(1000, 5);
+echo 'Final profit of weight product is: ' . $pr->countProfit() . PHP_EOL;
