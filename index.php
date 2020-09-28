7883a9bbb373c2ac1f2ce7bdc1dad300430b9485
Vogusov <vogusov@gmail.com>
2020-09-28 21:55:37 +0300

    singleton with trait

--- index.php
+++ index.php
@@ -1,43 +1,50 @@
 <?php
-
-abstract class Goods {
-  abstract public function getFinalPrice();
-}
-
-class DigitalGoods extends Goods {
-
-  public function getFinalPrice() {
-    return PieceGoods::getFinalPrice() / 2;
-  }
-}
-
-class PieceGoods extends Goods {
-  private $price;
-  private $quantity = 0;
-
-  public function __construct($price) {
-    $this->price = $price;
-  }
-
-
-  public function getFinalPrice() {
-    return $this->price * $this->quantity;
-  }
-}
-
-class WeightGoods extends Goods {
-
-  private $price;
-  private $weight;
-
-  public function __construct($price, $weight) {
-    $this->price = $price;
-    $this->weight = $weight;
-  }
-
-  public function getFinalPrice() {
-    return $this->price * $this->weight;
-  }
-
-
-}
+//
+//abstract class Product {
+//  const TAX = 0.13;
+//  abstract public function getFinalPrice();
+//  abstract public function countProfit();
+//}
+//
+//class DigitalProduct extends Product {
+//  const PRICE = 1000;
+//  public function getFinalPrice() {
+//    return PieceProduct::getFinalPrice() / 2;
+//  }
+//
+//  public function countProfit() {
+//    // TODO: Implement countProfit() method.
+//  }
+//}
+//
+//class PieceProduct extends Product {
+//  private $price;
+//  private $quantity = 1;
+//
+//  public function __construct($price) {
+//    $this->price = $price;
+//
+//  }
+//
+//
+//  public function getFinalPrice() {
+//    return $this->price * $this->quantity;
+//  }
+//}
+//
+//class WeightProduct extends Product {
+//
+//  private $price;
+//  private $weight;
+//
+//  public function __construct($price, $weight) {
+//    $this->price = $price;
+//    $this->weight = $weight;
+//  }
+//
+//  public function getFinalPrice() {
+//    return $this->price * $this->weight;
+//  }
+//
+//
+//}
