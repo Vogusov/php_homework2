6b5db54c21536046b34f6acdc9f193df87f45ad9
Vogusov <vogusov@gmail.com>
2020-10-21 16:03:47 +0300

    w/o twig, ajax, pdo

--- templates/gallery.php
+++ templates/gallery.php
@@ -6,15 +6,13 @@
   foreach ($items as $item):
   ?>
   <div style="border: 1px solid black">
-<<<<<<< HEAD
-=======
-    <p><?= $item['id'] ?></p>
-    <p><?= $item['name'] ?></p>
->>>>>>> f741fa8c1240bdc0f2d8745579d234ceba332059
+
+    <p><?= "{$item['name']} {$item['id']}" ?></p>
+
   </div>
 
   <?php endforeach; ?>
-  
+
   <a href="../controllers/index.php?show=<?= $count + $defaultGoodsCount ?>">Show more <?= $defaultGoodsCount ?>></a>
 
 </div>
